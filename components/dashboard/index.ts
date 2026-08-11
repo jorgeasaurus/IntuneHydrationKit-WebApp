d40d1e3eab0c2bf8ccf87bcf83036f5af7109b6a
Jorge Suarez <j0rg3@me.com>
2026-08-11 06:41:27 -0700

    Remove Graph and dashboard barrels

--- components/dashboard/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Dashboard components for execution monitoring
- */
-
-export { ProgressBar } from "./ProgressBar";
-export { TaskList } from "./TaskList";
-export { ExecutionControls } from "./ExecutionControls";
-export { ResultsSummary } from "./ResultsSummary";
-export { ActivityLog } from "./ActivityLog";
